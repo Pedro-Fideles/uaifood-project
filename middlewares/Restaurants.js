fe0eb0cae2969076961a606448698f915f8af7c9
Pedro Fideles <pedro.fideles162@gmail.vom>
2022-06-21 19:24:01 -0300

    feat: req. 1 - adiciona validações de localização e tipo

--- middlewares/Restaurants.js
+++ middlewares/Restaurants.js
@@ -1,6 +1,6 @@
 const { validateBr } = require('js-brasil');
 
-const { isDefined, isString, lengthIsSmallerThan } = require("./validations/validations");
+const { isDefined, isString, lengthIsSmallerThan, isEmpty } = require("./validations/validations");
 
 const errorStatus = 422;
 
@@ -20,6 +20,10 @@ const fieldErrors = {
   invalidFormat: (value) => ({
     code: errorStatus,
     message: `o campo ${value} está com um formato inválido.`
+  }),
+  fieldIsEmpty: (value) => ({
+    code: errorStatus,
+    message: `o campo ${value} não deve estar vazio`,
   }),
 }
 
@@ -56,8 +60,94 @@ const validatePassword = (req, _res, next) => {
   }
 }
 
+const validateAddress = (req, _res, next) => {
+  const { address } = req.body;
+
+  switch (true) {
+    case !isDefined(address): return next(fieldErrors.undefinedField('address'));
+    case !isString(address): return next(fieldErrors.notString('address'));
+    case lengthIsSmallerThan(address, 5): return next(fieldErrors.minLength('address', 5));
+    default: return next();
+  }
+}
+
+const validateNumber = (req, _res, next) => {
+  const { number } = req.body;
+
+  switch (true) {
+    case !isDefined(number): return next(fieldErrors.undefinedField('number'));
+    case !isString(number): return next(fieldErrors.notString('number'));
+    case isEmpty(number): return next(fieldErrors.fieldIsEmpty('number'));
+    default: return next();
+  }
+}
+
+const validateComplement = (req, _res, next) => {
+  const { complement } = req.body;
+
+  if (complement) {
+    switch (true) {
+      case !isString(complement): return next(fieldErrors.notString('complement'));
+      default: return next();
+    }
+  }
+
+  return next();
+}
+
+const validateDistrict = (req, _res, next) => {
+  const { district } = req.body;
+
+  switch (true) {
+    case !isDefined(district): return next(fieldErrors.undefinedField('district'));
+    case !isString(district): return next(fieldErrors.notString('district'));
+    case lengthIsSmallerThan(district, 5): return next(fieldErrors.minLength('district', 5));
+    default: return next();
+  }
+}
+
+const validateCity = (req, _res, next) => {
+  const { city } = req.body;
+
+  switch (true) {
+    case !isDefined(city): return next(fieldErrors.undefinedField('city'));
+    case !isString(city): return next(fieldErrors.notString('city'));
+    case lengthIsSmallerThan(city, 3): return next(fieldErrors.minLength('city', 3));
+    default: return next();
+  }
+}
+
+const validateState = (req, _res, next) => {
+  const { state } = req.body;
+
+  switch (true) {
+    case !isDefined(state): return next(fieldErrors.undefinedField('state'));
+    case !isString(state): return next(fieldErrors.notString('state'));
+    case lengthIsSmallerThan(state, 3): return next(fieldErrors.minLength('state', 3));
+    default: return next();
+  }
+}
+
+const validateType = (req, _res, next) => {
+  const { type } = req.body;
+
+  switch (true) {
+    case !isDefined(type): return next(fieldErrors.undefinedField('type'));
+    case !isString(type): return next(fieldErrors.notString('type'));
+    case lengthIsSmallerThan(type, 3): return next(fieldErrors.minLength('type', 3));
+    default: return next();
+  }
+}
+
 module.exports = {
   validateName,
   validateCNPJ,
   validatePassword,
+  validateAddress,
+  validateNumber,
+  validateComplement,
+  validateDistrict,
+  validateCity,
+  validateState,
+  validateType
 };
--- middlewares/validations/validations.js
+++ middlewares/validations/validations.js
@@ -2,10 +2,12 @@ const isDefined = (value) => value;
 const isString = (value) => typeof value === 'string';
 const isNumber = (value) => typeof value === 'number';
 const lengthIsSmallerThan = (value, num) => value.length < num;
+const isEmpty = (value) => value.length === 0;
 
 module.exports = {
   isDefined,
   isString,
   isNumber,
-  lengthIsSmallerThan
+  lengthIsSmallerThan,
+  isEmpty
 };

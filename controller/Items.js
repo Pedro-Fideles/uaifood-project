97799fb6e510c1773f66c4e4f2a57487508ad444
Pedro Fideles <pedro.fideles162@gmail.vom>
2022-06-22 15:33:35 -0300

    fix: ajustando a validação de token inválido

--- controller/Items.js
+++ controller/Items.js
@@ -1,8 +1,10 @@
 const Items = require('../services/Items');
 
-const createItem = async (req, res) => {
+const createItem = async (req, res, next) => {
   const { name, price, token, ingredients } = req.body;
-  const itemCreated = await Items.createItem({ name, price, token, ingredients });
+  const validation = await Items.createItem({ name, price, token, ingredients });
+
+  if (validation) next(validation);
 
   res.status(200).json({ message: 'deu certo' });
 };
